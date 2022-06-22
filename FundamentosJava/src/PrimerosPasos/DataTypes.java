46a77e159dec73314c701a8ec36afc5e1f6cd961
Daniel Rondon <danfelrondon@gmail.com>
2022-06-22 17:36:20 -0400

    Fundamentos de Java: Tipos de Datos Primitivos

---
+++ FundamentosJava/src/PrimerosPasos/DataTypes.java
@@ -0,0 +1,27 @@
+package PrimerosPasos;
+
+/**
+ * @author danfe
+ */
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Datos Primitivos
+        byte a = 126;
+        System.out.println("byte: " + a);
+        short b = 10000;
+        System.out.println("short: " + b);
+        int c = 123456789;
+        System.out.println("int: " + c);
+        long d = 9223372036854775805l;
+        System.out.println("long: " + d);
+        float e = 32.1451f;
+        System.out.println("float: " + e);
+        double f = 45.6547875435156465;
+        System.out.println("double: " + f);
+        char g = 'w';
+        System.out.println("char: " + g);
+        boolean h = true;
+        System.out.println("boolean: " + h);
+    }
+}
