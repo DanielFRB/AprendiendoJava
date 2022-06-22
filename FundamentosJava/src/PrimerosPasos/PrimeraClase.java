334cf9b9afa2665ed2e3def347f82fc4c9429137
Daniel Rondon <danfelrondon@gmail.com>
2022-06-22 11:04:01 -0400

    Fundamentos de Java: Primera Clase

---
+++ FundamentosJava/src/PrimerosPasos/PrimeraClase.java
@@ -0,0 +1,11 @@
+package PrimerosPasos;
+
+/**
+ * @author danfe
+ */
+
+public class PrimeraClase {
+    public static void main(String[] args) {
+        System.out.println("Hello World: This is my first class in Java");
+    }
+}
