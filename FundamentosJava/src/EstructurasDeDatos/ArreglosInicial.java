79f0792edb2ce097de6bdfb4d5af83efd3fc9350
Daniel Rondon <danfelrondon@gmail.com>
2022-06-24 22:20:59 -0400

    Fundamentos de Java: Estructuras de Datos: Arrays

---
+++ FundamentosJava/src/EstructurasDeDatos/ArreglosInicial.java
@@ -0,0 +1,26 @@
+package EstructurasDeDatos;
+
+import java.io.*;
+
+/**
+ * @author danfe
+ */
+
+public class ArreglosInicial {
+    public static void main(String [] a) throws IOException {
+        final int NUM = 10;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        int[] nums= new int[NUM];
+        int total = 1;
+        System.out.println("Por favor, introduzca " + NUM + " datos");
+        for (int i = 0; i < NUM; i++) {
+            nums[i] = Integer.parseInt(entrada.readLine());
+        }
+        System.out.println("Lista de números: ");
+        for (int i = 0; i < NUM; i++) {
+            System.out.print(" " + nums[i]);
+            total *= nums[i];
+        }
+        System.out.println("\nEl producto de los números es: " + total);
+    }
+}
---
+++ FundamentosJava/src/EstructurasDeDatos/ProductoElemArreglo.java
@@ -0,0 +1,43 @@
+package EstructurasDeDatos;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author danfe
+ */
+
+public class ProductoElemArreglo {
+    public static void main(String[] args) throws Exception {
+        double[] v;
+        int n;
+        System.out.println("Número de elementos: ");
+        n = Integer.parseInt(entrada.readLine());
+        v = new double[n];
+        leerArreglo(v);
+        System.out.println("El producto de los "+ n +" elementos es igual a: "+ productoArreglo(v));
+    }
+    
+    private static void leerArreglo(double[] a) throws Exception {
+        int n = 0;
+        System.out.println("Introduzca "+ a.length +" numeros.");
+        for (; n < a.length; n++) {
+            a[n] = Double.valueOf(entrada.readLine()).doubleValue();
+        }
+    }
+    
+    private static double productoArreglo(double[] w) {
+        double pd = 1.0;
+        int n = w.length - 1;
+        while (n > 0) {
+            if (w[n] > 0.0) {
+                pd *= w[n--];
+            } else {
+                n--;
+            }
+        }
+        return pd;
+    }
+    
+    static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+}
