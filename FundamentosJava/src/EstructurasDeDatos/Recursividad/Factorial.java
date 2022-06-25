f36f7f6985bd2de76cf0a18154c685caa08261a9
Daniel Rondon <danfelrondon@gmail.com>
2022-06-25 18:45:18 -0400

    Fundamentos de Java: Estructuras de Datos: Ejemplos de Recursividad

---
+++ FundamentosJava/src/EstructurasDeDatos/Recursividad/Factorial.java
@@ -0,0 +1,28 @@
+package EstructurasDeDatos.Recursividad;
+
+import java.io.*;
+
+/**
+ * @author danfe
+ */
+
+public class Factorial {
+    public static void main(String[] args) throws IOException{
+        int n;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        do {
+            System.out.println("Introduzca un numero 'n': ");
+            n = Integer.parseInt(entrada.readLine());
+        } while (n < 0);
+        System.out.println("\t"+ n +"! = "+ factorial(n));
+    }
+    
+    private static long factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            long result = n * factorial(n - 1);
+            return result;
+        }
+    }
+}
