39fc3547357efe78886d0797403c82e7e3b7fef4
Daniel Rondon <danfelrondon@gmail.com>
2023-02-01 19:46:30 -0400

    Commit 06: PildorasInformaticas - EstructurasPrincipales: Demo ejemplo de declaracion de tipos de datos primitivos en Java

---
+++ PildorasInformaticas/EstructurasPrincipales/src/com/dfrb/java/DemoTiposDeDatos.java
@@ -0,0 +1,28 @@
+package com.dfrb.java;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class DemoTiposDeDatos {
+    public static void main(String[] args) {
+        //Tipos de Datos Primitivos
+        System.out.println("Datos Primitivos");
+        byte a = 126;
+        System.out.println("byte: " + a);
+        short b = 10000;
+        System.out.println("short: " + b);
+        int c = 123456789;
+        System.out.println("int: " + c);
+        long d = 9223372036854775805L;
+        System.out.println("long: " + d);
+        float e = 32.1451f;
+        System.out.println("float: " + e);
+        double f = 45.6547875435156465;
+        System.out.println("double: " + f);
+        char g = 'w';
+        System.out.println("char: " + g);
+        boolean h = true;
+        System.out.println("boolean: " + h);
+    }
+}
