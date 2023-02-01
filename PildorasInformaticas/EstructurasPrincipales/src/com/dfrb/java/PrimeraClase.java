a8cea826fcf5cfc05d5210102939599a253ae96c
Daniel Rondon <danfelrondon@gmail.com>
2023-02-01 19:44:14 -0400

    Commit 05: PildorasInformaticas - EstructurasPrincipales: Agregando archivo PrimeraClase.java

---
+++ PildorasInformaticas/EstructurasPrincipales/src/com/dfrb/java/PrimeraClase.java
@@ -0,0 +1,11 @@
+package com.dfrb.java;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class PrimeraClase {
+    public static void main(String[] args) {
+        System.out.println("Buenos dias amigos !!!");
+    }
+}
