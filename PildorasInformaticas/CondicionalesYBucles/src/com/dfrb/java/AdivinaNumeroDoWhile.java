a5d0375a63d496b461c1e5dfbdc11cff38ba1753
Daniel Rondon <danfelrondon@gmail.com>
2023-02-03 12:02:09 -0400

    Commit 41: PildorasInformaticas - CondicionalesYBucles: Demo ejemplo de adivinar numero aleatorio entre 0 y 100, usando el bucle do-while

---
+++ PildorasInformaticas/CondicionalesYBucles/src/com/dfrb/java/AdivinaNumeroDoWhile.java
@@ -0,0 +1,28 @@
+package com.dfrb.java;
+
+import java.util.Scanner;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class AdivinaNumeroDoWhile {
+    public static void main(String[] args) {
+        int aleatorio = (int) (Math.random() * 100);
+        Scanner ent = new Scanner(System.in);
+        System.out.println("Adivina el numero...");
+        int numero = 0;
+        int intentos = 0;
+        do {
+            System.out.println("Ingresa un numero, por favor");
+            intentos++;
+            numero = ent.nextInt();
+            if (aleatorio < numero) {
+                System.out.println("El numero introducido es mayor, intenta de nuevo...");
+            } else if (aleatorio > numero) {
+                System.out.println("El numero introducido es menor, intenta de nuevo...");
+            }
+        } while (numero != aleatorio);
+        System.out.println("Haz adivinado el numero en "+ intentos +" intentos");
+    }
+}
