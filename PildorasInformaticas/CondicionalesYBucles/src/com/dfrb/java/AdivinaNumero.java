04eaab2adb36093e3689b158c5a693d974faaac8
Daniel Rondon <danfelrondon@gmail.com>
2023-02-02 21:56:42 -0400

    Commit 36: PildorasInformaticas - CondicionalesYBucles: Demo ejemplo de adivinar numero aleatorio entre 0 y 100, usando el bucle indeterminado while

---
+++ PildorasInformaticas/CondicionalesYBucles/src/com/dfrb/java/AdivinaNumero.java
@@ -0,0 +1,27 @@
+package com.dfrb.java;
+
+import java.util.Scanner;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class AdivinaNumero {
+    public static void main(String[] args) {
+        int aleatorio = (int) (Math.random() * 100);
+        Scanner ent = new Scanner(System.in);
+        System.out.println("Adivina el numero...");
+        int numero = 0;
+        int intentos = 0;
+        while (numero != aleatorio) {
+            System.out.println("Ingresa un numero, por favor");
+            numero = ent.nextInt();
+            if (aleatorio < numero) {
+                System.out.println("El numero introducido es mayor, intenta de nuevo...");
+            } else if (aleatorio > numero) {
+                System.out.println("El numero introducido es menor, intenta de nuevo...");
+            }
+        }
+        System.out.println("Haz adivinado el numero!!!");
+    }
+}
