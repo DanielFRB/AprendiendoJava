118ad3540de036284e155efa63a54f50602d24fc
Daniel Rondon <danfelrondon@gmail.com>
2023-02-03 12:54:07 -0400

    Commit 43: PildorasInformaticas - CondicionalesYBucles: Demo ejemplo uso de bucle for

---
+++ PildorasInformaticas/CondicionalesYBucles/src/com/dfrb/java/UsoBucleFor.java
@@ -0,0 +1,17 @@
+package com.dfrb.java;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class UsoBucleFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Daniel");
+        }
+        System.out.println("------------");
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Rondon");
+        }
+    }
+}
