698affa6a9d810a7f7aec4cfc115b6f22bf18116
Daniel Rondon <danfelrondon@gmail.com>
2023-02-03 22:00:14 -0400

    Commit 64: PildorasInformaticas - POO: Demo ejemplo clase UsoCoche3 instancia clase Coche3, utiliza metodos setter y solicita informacion por medio de JOptionPane

--- PildorasInformaticas/POO/src/com/dfrb/java/UsoCoche3.java
+++ PildorasInformaticas/POO/src/com/dfrb/java/UsoCoche3.java
@@ -1,13 +1,22 @@
 package com.dfrb.java;
 
+import javax.swing.JOptionPane;
+
 /**
  * @author dfrb@ne
  */
 
-public class UsoCoche2 {
+public class UsoCoche3 {
     public static void main(String[] args) {
-        Coche2 miCoche = new Coche2(); // Instanciar una Clase; Ejemplar de Clase
-        miCoche.estableceColor();
+        Coche3 miCoche = new Coche3(); // Instanciar una Clase; Ejemplar de Clase
+        miCoche.estableceColor(JOptionPane.showInputDialog("Introcule el color"));
         System.out.println(miCoche.dimeColor());
+        System.out.println(miCoche.dimeDatosGenerales());
+        miCoche.configuraAsientos(JOptionPane.showInputDialog("Tiene asientos de cuero?"));
+        System.out.println(miCoche.dimeAsientos());
+        miCoche.configuraClimatizador(JOptionPane.showInputDialog("Tiene climatizador?"));
+        System.out.println(miCoche.dimeClimatizador());
+        System.out.println(miCoche.dimePesoCoche());
+        System.out.println("El precio final del coche es: "+ miCoche.precioCoche());
     }
 }
