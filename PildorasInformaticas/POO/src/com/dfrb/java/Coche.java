e2e3ae7049d88930a5ba71b12820468321f3e80e
Daniel Rondon <danfelrondon@gmail.com>
2023-02-03 20:42:43 -0400

    Commit 59: PildorasInformaticas - POO: agregando clase Coche con campos-propiedades sin encapsular

---
+++ PildorasInformaticas/POO/src/com/dfrb/java/Coche.java
@@ -0,0 +1,21 @@
+package com.dfrb.java;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class Coche {
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    
+    public Coche() {
+        ruedas = 4;
+        largo = 500;
+        ancho = 200;
+        motor = 1600;
+        peso = 500;
+    }
+}
