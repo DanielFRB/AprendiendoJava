229e588ad207f017a36c77d4a1dfeecbfd43a4c2
Daniel Rondon <danfelrondon@gmail.com>
2023-02-04 21:38:32 -0400

    Commit 71: PildorasInformaticas - POO: Demo continuacion Pruebas.java, uso de la palabra static, definir una variable de clase estatica

--- PildorasInformaticas/POO/src/com/dfrb/java/Pruebas.java
+++ PildorasInformaticas/POO/src/com/dfrb/java/Pruebas.java
@@ -8,9 +8,13 @@ public class Pruebas {
     public static void main(String[] args) {
         EmpleadoPruebas trabajador1 = new EmpleadoPruebas("Paco");
         EmpleadoPruebas trabajador2 = new EmpleadoPruebas("Ana");
+        EmpleadoPruebas trabajador3 = new EmpleadoPruebas("Antonio");
+        EmpleadoPruebas trabajador4 = new EmpleadoPruebas("Maria");
         trabajador1.cambiaSeccion("RRHH");
         System.out.println(trabajador1.devuelveDatos());
         System.out.println(trabajador2.devuelveDatos());
+        System.out.println(trabajador3.devuelveDatos());
+        System.out.println(trabajador4.devuelveDatos());
     }
 }
 
@@ -18,6 +22,8 @@ class EmpleadoPruebas {
     public EmpleadoPruebas(String nom) {
         nombre = nom;
         seccion = "Administracion";
+        id = idSiguiente;
+        idSiguiente++;
     }
     
     public void cambiaSeccion(String seccion) {
@@ -25,9 +31,11 @@ class EmpleadoPruebas {
     }
     
     public String devuelveDatos() {
-        return "El nombre es: "+ nombre +" y la seccion es: "+ seccion;
+        return "El nombre es: "+ nombre +" la seccion es: "+ seccion +" y el Id es: "+ id;
     }
     
     private final String nombre;
     private String seccion;
+    private int id;
+    private static int idSiguiente = 1;
 }
