0d4d5699e36db357f2cc8a3c90c4647997c176b2
Daniel Rondon <danfelrondon@gmail.com>
2023-02-04 20:48:16 -0400

    Commit 67: PildorasInformaticas - POO: Demo ejemplo de construccion de objetos tipo empleado

---
+++ PildorasInformaticas/POO/src/com/dfrb/java/UsoEmpleado.java
@@ -0,0 +1,23 @@
+package com.dfrb.java;
+
+import java.util.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class UsoEmpleado {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Empleado {
+    public Empleado(String nom, double sue, int agno, int mes, int dia) {
+        
+    }
+    
+    private String nombre;
+    private double sueldo;
+    private Date altaContrato;
+}
