66a35fe11639474f24c3ed03c08822abeb778eb1
Daniel Rondon <danfelrondon@gmail.com>
2023-02-19 19:59:52 -0400

    Commit 165: PildorasInformaticas - AppsGraficas: Demo ejercicio practico: Aplicacion principal

---
+++ PildorasInformaticas/AppsGraficas/src/com/dfrb/pruebas/AplicacionPracticaDialogos.java
@@ -0,0 +1,16 @@
+package com.dfrb.pruebas;
+
+import javax.swing.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class AplicacionPracticaDialogos {
+    public static void main(String[] args) {
+        MarcoPracticoDialogos miMarco = new MarcoPracticoDialogos();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miMarco.setVisible(true);
+    }
+}
+
