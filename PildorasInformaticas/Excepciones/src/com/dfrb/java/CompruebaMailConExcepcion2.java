dabc84edc49bf3c80a139c7a575725c7c9799b5a
Daniel Rondon <danfelrondon@gmail.com>
2023-02-20 12:28:53 -0400

    Commit 177: PildorasInformaticas - Excepciones: Demo ejemplo donde se crea una excepcion propia o personalizada

---
+++ PildorasInformaticas/Excepciones/src/com/dfrb/java/CompruebaMailConExcepcion2.java
@@ -0,0 +1,50 @@
+package com.dfrb.java;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class CompruebaMailConExcepcion2 {
+    public static void main(String[] args) {
+        String mail = JOptionPane.showInputDialog("Introduce tu mail");
+        try {
+            examinaEmail(mail);
+        } catch (LongitudMailErronea e) {
+            System.err.println("El mail es demasiado corto");
+        }
+    }
+    
+    static void examinaEmail(String email) throws LongitudMailErronea {
+        int arroba = 0;
+        boolean punto = false;
+        if (email.length() <= 3) {
+            throw new LongitudMailErronea();
+        } else {
+            for (int i = 0; i < email.length(); i++) {
+                if (email.charAt(i) == '@') {
+                    arroba++;
+                }
+                if (email.charAt(i) == '.') {
+                    punto = true;
+                }
+            }
+            if (arroba == 1 && punto) {
+                System.out.println("El mail es correcto");
+            } else {
+                System.out.println("El mail no es correcto");
+            }
+        }
+    }
+}
+
+class LongitudMailErronea extends Exception {
+    public LongitudMailErronea() {}
+    
+    public LongitudMailErronea(String msjError) {
+        super(msjError);
+    }
+}
