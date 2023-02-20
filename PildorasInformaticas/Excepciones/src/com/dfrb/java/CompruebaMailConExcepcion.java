6f01078689149a1583412aa429ccaa751467cc0e
Daniel Rondon <danfelrondon@gmail.com>
2023-02-20 11:20:03 -0400

    Commit 176: PildorasInformaticas - Excepciones: Demo ejemplo donde se utiliza la clausula throw para obligar al programa a lanzar una excepcion

--- PildorasInformaticas/Excepciones/src/com/dfrb/java/CompruebaMailConExcepcion.java
+++ PildorasInformaticas/Excepciones/src/com/dfrb/java/CompruebaMailConExcepcion.java
@@ -1,28 +1,37 @@
 package com.dfrb.java;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 /**
  * @author dfrb@ne
  */
 
-public class CompruebaMail_2 {
+public class CompruebaMailConExcepcion {
     public static void main(String[] args) {
+        String mail = JOptionPane.showInputDialog("Introduce tu mail");
+        examinaEmail(mail);
+    }
+    
+    static void examinaEmail(String email) throws ArrayIndexOutOfBoundsException {
         int arroba = 0;
         boolean punto = false;
-        String mail = JOptionPane.showInputDialog("Introduce tu mail");
-        for (int i = 0; i < mail.length(); i++) {
-            if (mail.charAt(i) == '@') {
-                arroba++;
-            }
-            if (mail.charAt(i) == '.') {
-                punto = true;
-            }
-        }
-        if (arroba == 1 && punto) {
-            System.out.println("El mail es correcto");
+        // Con el condicional if se evalua la cantidad de letras introducido por parte del usuario
+        if (email.length() <= 3) { // Aqui estanos obligando a nuestro programa a lanzar una excepcion
+            throw new ArrayIndexOutOfBoundsException();
         } else {
-            System.out.println("El mail no es correcto");
+            for (int i = 0; i < email.length(); i++) {
+                if (email.charAt(i) == '@') {
+                    arroba++;
+                }
+                if (email.charAt(i) == '.') {
+                    punto = true;
+                }
+            }
+            if (arroba == 1 && punto) {
+                System.out.println("El mail es correcto");
+            } else {
+                System.out.println("El mail no es correcto");
+            }
         }
     }
 }
