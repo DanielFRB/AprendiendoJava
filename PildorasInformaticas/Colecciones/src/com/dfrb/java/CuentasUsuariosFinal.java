92cc57c1e47f84ab8f7423ea393041d13049081a
Daniel Rondon <danfelrondon@gmail.com>
2023-02-28 20:24:12 -0400

    Commit 219: PildorasInformaticas - Colecciones: Demo ejemplo de comparacion de objetos de tipo ClienteFinal, el ultimo objeto no se agrega debido a que es igual al primero clt1=clt5, por consiguiente la instancia clientesBanco de tipo Set no permite objetos repetidos

--- PildorasInformaticas/Colecciones/src/com/dfrb/java/CuentasUsuariosFinal.java
+++ PildorasInformaticas/Colecciones/src/com/dfrb/java/CuentasUsuariosFinal.java
@@ -6,20 +6,23 @@ import java.util.*;
  * @author dfrb@ne
  */
 
-public class CuentasUsuarios {
+public class CuentasUsuariosFinal {
     public static void main(String[] args) {
-        Cliente clt1 = new Cliente("Daniel Rondon", "987654", 2000);
-        Cliente clt2 = new Cliente("Oswaldo Lopez", "321654", 4000);
-        Cliente clt3 = new Cliente("Yaneth Machado", "951357", 5000);
-        Cliente clt4 = new Cliente("Barack Obama", "951951", 15000);
+        ClienteFinal clt1 = new ClienteFinal("Daniel Rondon", "987654", 2000);
+        ClienteFinal clt2 = new ClienteFinal("Oswaldo Lopez", "321654", 4000);
+        ClienteFinal clt3 = new ClienteFinal("Yaneth Machado", "951357", 5000);
+        ClienteFinal clt4 = new ClienteFinal("Barack Obama", "951951", 15000);
+        // Agregando codigo nuevo
+        ClienteFinal clt5 = new ClienteFinal("Daniel Rondon", "987654", 2000);
         
-        Set<Cliente> clientesBanco = new HashSet<>();
+        Set<ClienteFinal> clientesBanco = new HashSet<>();
         clientesBanco.add(clt1);
         clientesBanco.add(clt2);
         clientesBanco.add(clt3);
         clientesBanco.add(clt4);
+        clientesBanco.add(clt5);
         
-        for (Cliente cliente : clientesBanco) {
+        for (ClienteFinal cliente : clientesBanco) {
             System.out.println(cliente.getNombre() +" "+ cliente.getNumCuenta()+" "+ cliente.getSaldo());
         }
     }
