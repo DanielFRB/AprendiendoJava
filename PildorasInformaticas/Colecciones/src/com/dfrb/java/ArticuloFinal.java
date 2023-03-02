d8031abc2e2c667fac5eccdce65daaf3e65ff04f
Daniel Rondon <danfelrondon@gmail.com>
2023-03-01 21:42:54 -0400

    Commit 226: PildorasInformaticas - Colecciones: Demo cont. Articulo3.java y PruebaTreeSet3.java, Se elimina la clase adicional que implementa Comparator, se manejan clases anonimas internas y expresiones lambdas -- Se renombra a ArticuloFinal y PruebaTreeSetFinal

---
+++ PildorasInformaticas/Colecciones/src/com/dfrb/java/ArticuloFinal.java
@@ -0,0 +1,26 @@
+package com.dfrb.java;
+
+/**
+ * @author dfrb@ne
+ */
+
+// Como ultima modificacion se elimina la clase ComparadorArticulos que se utilizo en el archivo Articulo3.java,
+// quedando la Clase ArticuloFinal con el mismo codigo que la Clase Articulo1, del archivo Articulo1.java
+public class ArticuloFinal implements Comparable<ArticuloFinal> {
+    public ArticuloFinal(int num, String desc) {
+        this.numeroArticulo = num;
+        this.descripcion = desc;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    @Override
+    public int compareTo(ArticuloFinal articulo) {
+        return numeroArticulo - articulo.numeroArticulo;
+    }
+    
+    private int numeroArticulo;
+    private String descripcion;
+}
---
+++ PildorasInformaticas/Colecciones/src/com/dfrb/java/PruebaTreeSetFinal.java
@@ -0,0 +1,67 @@
+package com.dfrb.java;
+
+import java.util.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class PruebaTreeSetFinal {
+    public static void main(String[] args) {
+        TreeSet<String> personas = new TreeSet<>();
+        personas.add("Daniel");
+        personas.add("Yaneth");
+        personas.add("Oswaldo");
+        personas.add("Barack");
+        personas.add("Adelina");
+        
+        System.out.println("Ordenados Alfabeticamente por el metodo compareTo() de la Clase String.");
+        for (String p : personas) {
+            System.out.println(p);
+        }
+        System.out.println("------------------------");
+        
+        ArticuloFinal primero = new ArticuloFinal(1, "Primer Articulo");
+        ArticuloFinal segundo = new ArticuloFinal(20, "Segundo Articulo");
+        ArticuloFinal tercero = new ArticuloFinal(3, "Tercer Articulo");
+        
+        TreeSet<ArticuloFinal> listaArticulos = new TreeSet<>();
+        listaArticulos.add(primero);
+        listaArticulos.add(segundo);
+        listaArticulos.add(tercero);
+        
+        System.out.println("Ordenados numericamente por numeroArticulo por el metodo compareTo() de la Clase ArticuloFinal.");
+        for (ArticuloFinal a : listaArticulos) {
+            System.out.println(a.getDescripcion());
+        }
+        System.out.println("------------------------");
+        
+        // Usando clases anonimas internas
+        TreeSet<ArticuloFinal> listaArticulos2 = new TreeSet<>(new Comparator<ArticuloFinal>() {
+            @Override
+            public int compare(ArticuloFinal art1, ArticuloFinal art2) {
+                return art1.getDescripcion().compareTo(art2.getDescripcion());
+            }
+        });
+        listaArticulos2.add(primero);
+        listaArticulos2.add(segundo);
+        listaArticulos2.add(tercero);
+        
+        System.out.println("Ordenados alfabeticamente por descripcion, por el metodo compare() de la Clase Anonima Interna.");
+        for (ArticuloFinal a : listaArticulos2) {
+            System.out.println(a.getDescripcion());
+        }
+        System.out.println("------------------------");
+        
+        // Usando Expresiones Lambdas, se simplifica aun mas la clase anonima interna.
+        TreeSet<ArticuloFinal> listaArticulos3 = new TreeSet<>((art1, art2) -> art1.getDescripcion().compareTo(art2.getDescripcion()));
+        listaArticulos3.add(primero);
+        listaArticulos3.add(segundo);
+        listaArticulos3.add(tercero);
+        
+        System.out.println("Ordenados alfabeticamente por descripcion, por el metodo compareTo() de la Expresion Lambda.");
+        for (ArticuloFinal a : listaArticulos3) {
+            System.out.println(a.getDescripcion());
+        }
+    }
+}
