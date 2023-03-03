2e50d79aa112af7a6f02b674a311e4ddd359fe7e
Daniel Rondon <danfelrondon@gmail.com>
2023-03-02 21:25:43 -0400

    Commit 235: PildorasInformaticas - JavaSockets: Clase Principal Cliente. ejecuta la aplicacion Cliente y carga el MarcoCliente

---
+++ PildorasInformaticas/JavaSockets/src/com/dfrb/java/Cliente.java
@@ -0,0 +1,15 @@
+package com.dfrb.java;
+
+import javax.swing.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class Cliente {
+    public static void main(String[] args) {
+        MarcoCliente cliente = new MarcoCliente();
+        cliente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cliente.setVisible(true);
+    }
+}
