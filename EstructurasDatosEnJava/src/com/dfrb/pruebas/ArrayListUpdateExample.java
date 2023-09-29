ffe7dbe0d6c98a13573a03f17bdf7bde41f2f8c5
Daniel Rondon <danfelrondon@gmail.com>
2023-09-29 17:46:40 -0400

    Commit 387: EstructurasDatosEnJava - Pruebas - ArrayListUpdateExample: actualizacion de demo ejemplo que nos permite actualizar el contenido de un ArrayList

--- EstructurasDatosEnJava/src/com/dfrb/pruebas/ArrayListUpdateExample.java
+++ EstructurasDatosEnJava/src/com/dfrb/pruebas/ArrayListUpdateExample.java
@@ -8,25 +8,25 @@ import java.util.List;
  */
 
 public class ArrayListUpdateExample {
-  public static void main(String[] args) {
-    List<Integer> lista = new ArrayList<>();
+    public static void main(String[] args) {
+        List<Integer> lista = new ArrayList<>();
     
-    lista.add(4);
-    lista.add(40);
-    lista.add(14);
-    lista.add(80);
-    lista.add(124);
+        lista.add(5);
+        lista.add(20);
+        lista.add(14);
+        lista.add(80);
+        lista.add(124);
     
-    System.out.println("Lista values before update - " + lista);
-    System.out.println("Lista size before - " + lista.size());
+        System.out.println("Lista values before update - " + lista);
+        System.out.println("Lista size before - " + lista.size());
     
-    for (int index = 0; index < lista.size(); index++) {
-      if (lista.get(index) % 5 == 0) {
-        lista.set(index, -1);
-      }
-    }
+        for (int index = 0; index < lista.size(); index++) {
+            if (lista.get(index) % 5 == 0) {
+                lista.set(index, -1);
+            }
+        }
 
-    System.out.println("Lista values after updating index with new values - "+ lista);
-    System.out.println("Lista size after - "+ lista.size());
-  }
+        System.out.println("Lista values after updating index with new values - "+ lista);
+        System.out.println("Lista size after - "+ lista.size());
+    }
 }
