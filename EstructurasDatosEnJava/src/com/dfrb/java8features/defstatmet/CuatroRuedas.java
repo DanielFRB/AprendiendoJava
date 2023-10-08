38937baeaa4f0cbcbca5c195ab7a4419e5bec3ad
Daniel Rondon <danfelrondon@gmail.com>
2023-10-08 18:00:30 -0400

    Commit 391: EstructurasDatosEnJava - Java8Features - DefaultStaticMethod: agregando interface CautroRuedas.java con un default metodo printWheelsType()

---
+++ EstructurasDatosEnJava/src/com/dfrb/java8features/defstatmet/CuatroRuedas.java
@@ -0,0 +1,11 @@
+package com.dfrb.java8features.defstatmet;
+
+/**
+ * @author dfrb@ne
+ */
+
+public interface CuatroRuedas {
+    default String printWheelsType() {
+        return "Cuatro ruedas";
+    }
+}
