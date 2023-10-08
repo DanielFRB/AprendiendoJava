8732e0e7fd2c018043afef6d312580f6755c5929
Daniel Rondon <danfelrondon@gmail.com>
2023-10-08 18:00:04 -0400

    Commit 390: EstructurasDatosEnJava - Java8Features - DefaultStaticMethod: agregando interface DosRuedas.java con un default metodo printWheelsType()

---
+++ EstructurasDatosEnJava/src/com/dfrb/java8features/defstatmet/DosRuedas.java
@@ -0,0 +1,11 @@
+package com.dfrb.java8features.defstatmet;
+
+/**
+ * @author dfrb@ne
+ */
+
+public interface DosRuedas {
+    default String printWheelsType() {
+        return "Dos ruedas";
+    }
+}
