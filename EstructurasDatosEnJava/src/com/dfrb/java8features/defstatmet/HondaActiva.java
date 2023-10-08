edadd5df6b7738c3527d3d53d257d251ad664444
Daniel Rondon <danfelrondon@gmail.com>
2023-10-08 18:01:53 -0400

    Commit 392: EstructurasDatosEnJava - Java8Features - DefaultStaticMethod: agregando clase HondaActiva que implementa la interface DosRuedas.java

---
+++ EstructurasDatosEnJava/src/com/dfrb/java8features/defstatmet/HondaActiva.java
@@ -0,0 +1,12 @@
+package com.dfrb.java8features.defstatmet;
+
+
+/**
+ * @author dfrb@ne
+ */
+
+public class HondaActiva implements DosRuedas {
+    public void printName() {
+        System.out.println("Esta es la Honda Activa 7G de 500cc y es un Vehiculo de "+ printWheelsType());
+    }
+}
