69f14cf840512bc7030ed6959a998a0b357e91e5
Daniel Rondon <danfelrondon@gmail.com>
2023-10-08 18:20:36 -0400

    Commit 397: EstructurasDatosEnJava - Java8Features - DefaultStaticMethod: Demo ejemplo de como funcional los metodos default y static de una interface y su comportamiento dentro de la Clase que la implementa.

---
+++ EstructurasDatosEnJava/src/com/dfrb/java8features/defstatmet/DefaultStaticClassDemo.java
@@ -0,0 +1,18 @@
+package com.dfrb.java8features.defstatmet;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class DefaultStaticClassDemo implements DefaultStaticInterfaceDemo {
+    public static void main(String[] args) {
+        // El metodo static display() se llama directamente con el nombre de la interface
+        DefaultStaticInterfaceDemo.display();
+        
+        // Se crea un objeto de tipo DefaultStaticClassDemo 
+        DefaultStaticClassDemo defaultStaticClassDemo = new DefaultStaticClassDemo();
+        
+        // Se ejecuta el metodo default show() por medio del objeto creado
+        defaultStaticClassDemo.show();
+    }
+}
---
+++ EstructurasDatosEnJava/src/com/dfrb/java8features/defstatmet/DefaultStaticInterfaceDemo.java
@@ -0,0 +1,15 @@
+package com.dfrb.java8features.defstatmet;
+
+/**
+ * @author dfrb@ne
+ */
+
+public interface DefaultStaticInterfaceDemo {
+    default void show() {
+      System.out.println("In Java 8 - default method - DefaultStaticInterfaceDemo");
+    }
+    
+    static void display() {
+      System.out.println("In DefaultStaticInterfaceDemo I");
+    }
+}
