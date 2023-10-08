744c1b6c1662b8606b9544669cbf0b56597ea8f9
Daniel Rondon <danfelrondon@gmail.com>
2023-10-08 18:06:42 -0400

    Commit 394: EstructurasDatosEnJava - Java8Features - DefaultStaticMethod: agregando clase con metodo main() DefaultMethods.java, donde se ejecuta el ejemplo de metodos default.

---
+++ EstructurasDatosEnJava/src/com/dfrb/java8features/defstatmet/DefaultMethods.java
@@ -0,0 +1,14 @@
+package com.dfrb.java8features.defstatmet;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class DefaultMethods {
+    public static void main(String[] args) {
+        HondaActiva moto = new HondaActiva();
+        moto.printName();
+        UtilidadRuedas ur = new UtilidadRuedas();
+        ur.printUtilities();
+    }
+}
